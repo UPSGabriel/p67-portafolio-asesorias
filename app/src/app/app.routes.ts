11e37658bfbab719a23d03042f4477f1a7818bac
Patricio Pacheco Sarmiento <danielpacheco842005@gmail.com>
2025-11-23 20:54:15 -0500

    Daniel: componente public creado y rutas configuradas

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login.component';
 
+
+
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  // aquí luego agregaremos /public, /programmer, /admin
+
+  // RUTA PÚBLICA (la tuya)
+   {
+    path: 'public',
+    loadComponent: () =>
+      import('./public/public.component').then(m => m.Public),
+  }
+
+
+  // más rutas vendrán luego:
+  // { path: 'admin', ... }
+  // { path: 'programmer', ... }
 ];
---
+++ app/src/app/public/public.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public',
+  imports: [],
+  templateUrl: './public.component.html',
+  styleUrl: './public.component.scss',
+})
+export class Public {
+  proyectos = [
+    {
+      titulo: 'Sistema de Gestión',
+      descripcion: 'Proyecto escolar con Angular y Firebase.',
+      imagen: 'https://source.unsplash.com/random/400x300?code'
+    },
+    {
+      titulo: 'Portfolio Web',
+      descripcion: 'Página personal con diseño responsive.',
+      imagen: 'https://source.unsplash.com/random/400x300?developer'
+    },
+    {
+      titulo: 'E-Commerce Demo',
+      descripcion: 'Tienda online usando Angular + Stripe.',
+      imagen: 'https://source.unsplash.com/random/400x300?shop'
+    }
+  ];
+
+  habilidades = [
+    'Angular',
+    'Firebase',
+    'TypeScript',
+    'HTML',
+    'CSS / SCSS',
+    'Responsive Design',
+  ];
+}
